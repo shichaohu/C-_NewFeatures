cce9abf349a2a8ec92e776294f9a18fcd81cb89f
shichaohu <502242999@qq.com>
2017-10-26 17:30:56 +0800

    C# 6.0及其以后的新特性

---
+++ Csharp_NewFeatures/Csharp_NewFeatures/Csharp 6.0.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp_NewFeatures
+{
+    public class Csharp_6
+    {
+        public void Init()
+        {
+            //一、字符串插值
+            Person p = new Person { FirstName = "Jack", LastName = "Wang", Age = 100 };
+            var results = $"First Name: {p.FirstName} LastName: {p.LastName} Age: {p.Age}";
+        }
+
+    }
+
+    class Person
+    {
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public int Age { set; get; }
+    }
+}
